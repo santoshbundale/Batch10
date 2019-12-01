01c625b8b601884c419d6d2c8309b6a7f855d454
santosh <santoshbundale@gmail.com>
2019-12-01 13:05:28 +0530

    A project is created

---
+++ GitDemo/GitDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to GIT");
+            Console.ReadKey();
+        }
+    }
+}
